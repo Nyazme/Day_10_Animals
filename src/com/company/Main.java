db15c01b742c0316fdbecaa7aca22fb7b779f647
Mark Feinman <feinmanmarke@gmail.com>
2018-01-22 16:39:05 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog Brutus = new Dog ("Brutus", "Woof");
+        Cat Whiskers = new Cat ("Whiskers", "Meow");
+        Brutus.speak();
+        Whiskers.speak();
+    }
+
+
+}
+
+
